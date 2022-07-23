c8b842f8038175c4be1f098d59b0092490987298
Alexander Zhukov <zhukoff.alexander@gmail.com>
2022-07-23 10:09:05 +0300

    feat: Add a button, paragraph, Htag component
    Set up the webpack to work with the svg file as a component

---
+++ components/P/P.props.ts
@@ -0,0 +1,10 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+export interface IPProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLParagraphElement>,
+    HTMLParagraphElement
+  > {
+  children: ReactNode;
+  size?: 'small' | 'medium' | 'large';
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
 import Image from 'next/image';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
-  return <div>Подключенный шрифт</div>;
+  return (
+    <>
+      <Htag tag="h1">Подключенный шрифт</Htag>
+      <Button arrow="none" appearance="primary">
+        Кнопка
+      </Button>
+      <Button arrow="right" appearance="ghost" className="fadfdsafd">
+        Кнопка
+      </Button>
+      <P style={{ color: 'red' }} size="large">
+        Красный параграф
+      </P>
+    </>
+  );
 }
