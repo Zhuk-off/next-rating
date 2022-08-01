b12acd0a638c3fe2f967c47ee80bdee8c02c0c1c
Alexander Zhukov <zhukoff.alexander@gmail.com>
2022-08-01 19:26:38 +0300

    feat: Make a product page layout
    Create components Card, Advantages, HhData(job counter component). Page header layout. Make an adaptive layout of the vacancy counter components. Use regexp to display price

---
+++ components/Card/Card.tsx
@@ -0,0 +1,21 @@
+import { ICardProps } from './Card.props';
+import styles from './Card.module.css';
+import cn from 'classnames';
+
+export const Card = ({
+  color = 'white',
+  children,
+  className,
+  ...props
+}: ICardProps): JSX.Element => {
+  return (
+    <div
+      className={cn(styles.card, className, {
+        [styles.blue]: color == 'blue',
+      })}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
--- page-components/TopPageComponent/TopPageComponenet.tsx
+++ page-components/TopPageComponent/TopPageComponenet.tsx
@@ -1,5 +1,8 @@
 import styles from './TopPageComponent.module.css';
 import { ITopPageComponentProps } from './TopPageComponenet.props';
+import { Advantages, HhData, Htag, P, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import PageLoader from 'next/dist/client/page-loader';
 
 export const TopPageComponent = ({
   firstCategory,
@@ -7,8 +10,43 @@ export const TopPageComponent = ({
   products,
 }: ITopPageComponentProps): JSX.Element => {
   return (
-    <>
-      <p>{products && products.length}</p>
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="medium">
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортировка</span>
+      </div>
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        {products && (
+          <Tag color="red" size="medium">
+            hh.ru
+          </Tag>
+        )}
+      </div>
+      {firstCategory == TopLevelCategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
+      {page.advantages && page.advantages.length > 0 && (
+        <>
+          <Htag tag="h2">Преимущества</Htag>
+          <Advantages advantages={page.advantages} />
+        </>
+      )}
+      {page.seoText && <P>{page.seoText}</P>}
+      <Htag tag="h2">Получаемые навыки</Htag>
+      {page.tags.map((t) => (
+        <Tag key={t} color="primary">
+          {t}
+        </Tag>
+      ))}
+    </div>
   );
 };
